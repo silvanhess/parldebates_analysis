8316196b8c5551e83471d214296aaca3ddec2bb7
Silvan Hess <silvan.hess@stud.unilu.ch>
2025-12-05 16:44:58 +0100

    complete script for downloading the transcript, using parrallel api requests and caching

--- Scripts/download_transcripts.R
+++ Scripts/download_transcripts.R
@@ -7,30 +7,36 @@ library(tidytext)
 # ?tidytext
 library(rlang)
 library(furrr)
+library(future)
+
+# set up parallel --------------------------------------------------------
+
+# set up parallel processing with background R sessions
+plan(multisession, workers = 6)
 
 # check package content --------------------------------------------------
 
-get_tables()
-overview <- get_overview()
-overview |> filter(variable == "IdSession")
-get_variables("Subject")
-sample_subjects <- get_glimpse("Subject", rows = 100)
-get_variables("SubjectBusiness")
-sample_subjectbusiness <- get_glimpse("SubjectBusiness", rows = 100)
-get_variables("Business")
-sample_business <- get_glimpse("Business", rows = 100)
-get_variables("Session")
-sample_sessions <- get_glimpse("Session", rows = 100)
-get_variables("Meeting")
-sample_meetings <- get_glimpse("Meeting", rows = 100)
-get_variables("Transcript")
-sample_transcripts <- get_glimpse("Transcript", rows = 100)
-get_variables("Session")
-sample_sessions <- get_glimpse("Session", rows = 100)
+# get_tables()
+# overview <- get_overview()
+# overview |> filter(variable == "IdSession")
+# get_variables("Subject")
+# sample_subjects <- get_glimpse("Subject", rows = 100)
+# get_variables("SubjectBusiness")
+# sample_subjectbusiness <- get_glimpse("SubjectBusiness", rows = 100)
+# get_variables("Business")
+# sample_business <- get_glimpse("Business", rows = 100)
+# get_variables("Session")
+# sample_sessions <- get_glimpse("Session", rows = 100)
+# get_variables("Meeting")
+# sample_meetings <- get_glimpse("Meeting", rows = 100)
+# get_variables("Transcript")
+# sample_transcripts <- get_glimpse("Transcript", rows = 100)
+# get_variables("Session")
+# sample_sessions <- get_glimpse("Session", rows = 100)
 
-?get_data
+# ?get_data
 
-# get businesses -------------------------------------------
+# tests ------------------------------------------------------------------
 
 # business_25041 <- get_data(
 #   "Business",
@@ -53,173 +59,173 @@ sample_sessions <- get_glimpse("Session", rows = 100)
 #   Language = "DE"
 # )
 
-year = 2015:2025
-businesses_list <- list()
-for (i in seq_along(year)) {
-  businesses_list[[i]] <- get_data(
-    "Business",
-    SubmissionDate = c(
-      paste(">", year[i], "-01-01", sep = ""),
-      paste("<", year[i], "-12-31", sep = "")
-    ),
-    Language = "DE"
-  )
-}
-businesses <- bind_rows(businesses_list)
-saveRDS(businesses, "Data/businesses_2015_2025.rds")
+# get businesses -------------------------------------------
 
 # für alle Geschäfte, die im Parlament von 2015-2025 behandelt wurden,
 # sind auch Geschäfte nötig, die bereits vor 2015 eingereicht wurden.
+# Daher werden hier alle Geschäfte von 2010-2025 heruntergeladen.
+
+yr = 2010:2025
+
+# with walk
+
+get_businesses_year <- function(yr) {
+  folder <- "Data/businesses"
+  if (!dir.exists(folder)) {
+    dir.create(folder)
+  }
+
+  out_file <- file.path(folder, paste0(yr, ".rds"))
+  if (file.exists(out_file)) {
+    return(NULL)
+  } # skip existing files
+
+  dt <- get_data(
+    "Business",
+    SubmissionDate = c(
+      paste(">", yr, "-01-01", sep = ""),
+      paste("<", yr, "-12-31", sep = "")
+    ),
+    Language = "DE"
+  )
+
+  saveRDS(dt, file.path(folder, paste0(yr, ".rds")))
+}
+
+walk(
+  yr,
+  get_businesses_year,
+  .progress = TRUE
+)
+
+businesses <- map_dfr(list.files("Data/businesses", full.names = T), readRDS)
+saveRDS(businesses, "Data/businesses_2015_2025.rds")
+
+# Nur Geschäfte, welche im Parlament behandelt wurden
+# Aschliessen: Anfrage, Dringliche Anfrage, Petition, Interpellation, Fragestunde
+
+# count business types
+businesses |>
+  count(BusinessType, BusinessTypeName) |>
+  arrange(n) |>
+  print(n = Inf)
+
+# filter business types
+businesses_filtered <- businesses |>
+  filter(!(BusinessType %in% c(18, 19, 10, 8, 14)))
+
+saveRDS(businesses_filtered, "Data/businesses_filtered_2015_2025.rds")
 
 # get subject businesses --------------------------------------------------------
 
-# with for loop
-
-# subject_businesses_list <- list()
-# # for (i in seq_along(businesses$BusinessShortNumber)) {
-# for (i in 637:length(businesses$BusinessShortNumber)) {
-#   subject_businesses_list[[i]] <- get_data(
-#     table = "SubjectBusiness",
-#     BusinessShortNumber = businesses$BusinessShortNumber[i],
-#     Language = "DE"
-#   )
-#   paste(
-#     "item",
-#     businesses$BusinessShortNumber[i],
-#     "of",
-#     length(businesses$BusinessShortNumber),
-#     "done"
-#   ) |>
-#     print()
-# }
-
-# subject_businesses <- bind_rows(subject_businesses_list)
-# saveRDS(subject_businesses, "Data/subject_businesses_2015_2025.rds")
-
 # with walk
 
-get_subject_business <- function(bsn) {
+get_subject_business <- function(bsn, retries = 5, wait = 3) {
   folder <- "Data/subject_businesses"
-  # folder <- "Data/subject_businesses_parallel"
   if (!dir.exists(folder)) {
     dir.create(folder)
   }
 
-  dt <- get_data(
-    table = "SubjectBusiness",
-    BusinessShortNumber = bsn,
-    Language = "DE"
-  )
+  out_file <- file.path(folder, paste0(bsn, ".rds"))
+  if (file.exists(out_file)) {
+    return(NULL)
+  } # skip existing files
 
-  saveRDS(dt, file.path(folder, paste0(bsn, ".rds")))
+  for (attempt in 1:retries) {
+    result <- try(
+      get_data(
+        table = "SubjectBusiness",
+        BusinessShortNumber = bsn,
+        Language = "DE"
+      ),
+      silent = TRUE
+    )
+
+    if (!inherits(result, "try-error")) {
+      saveRDS(result, out_file)
+      message("Downloaded ", bsn, " (attempt ", attempt, ")")
+      return(TRUE)
+    }
+
+    message("Error on ", bsn, " (attempt ", attempt, "): ", result)
+    Sys.sleep(wait * attempt) # exponential backoff
+  }
+
+  message("FAILED permanently: ", bsn)
+  write(bsn, file = "failed_ids.txt", append = TRUE)
+  return(FALSE)
 }
 
-# walk(
-#   businesses$BusinessShortNumber,
-#   get_subject_business,
-#   .progress = TRUE
-# )
-
-plan(multisession, workers = 6)
-future_walk(
+# Start parallel jobs
+fut <- future_walk(
   businesses$BusinessShortNumber,
   get_subject_business,
   .progress = TRUE
 )
 
+# WAIT UNTIL ALL JOBS ARE FINISHED
+value(fut)
+
 subject_businesses <- map_dfr(
-  list.files("subject_businesses", full.names = TRUE),
+  list.files("Data/subject_businesses", full.names = TRUE),
   readRDS
 )
 saveRDS(subject_businesses, "Data/subject_businesses_2015_2025.rds")
-
-# clean the dataset
-# we just want the subjects that have discussions in the parliament
 
 # get the transcripts ----------------------------------------------------
 
-# with for loop
-
-transcripts_list <- list()
-for (i in seq_along(subject_businesses$IdSubject)) {
-  transcripts_list[[i]] <- get_data(
-    table = "Transcript",
-    IdSubject = subject_businesses$IdSubject[i],
-    Language = "DE"
-  )
-  paste(
-    "item",
-    subject_businesses$IdSubject[i],
-    "of",
-    length(subject_businesses$IdSubject),
-    "done"
-  ) |>
-    print()
-}
-transcripts <- bind_rows(transcripts_list)
-saveRDS(transcripts, "Data/transcripts_energy.rds")
-
 # with walk
 
-get_transcripts <- function(bsn) {
-  folder <- "Data/subject_businesses"
-  # folder <- "Data/subject_businesses_parallel"
+get_transcripts <- function(sbj, retries = 5, wait = 3) {
+  folder <- "Data/transcripts"
   if (!dir.exists(folder)) {
     dir.create(folder)
   }
 
-  dt <- get_data(
-    table = "SubjectBusiness",
-    BusinessShortNumber = bsn,
-    Language = "DE"
-  )
+  out_file <- file.path(folder, paste0(sbj, ".rds"))
+  if (file.exists(out_file)) {
+    return(NULL)
+  } # skip existing files
 
-  saveRDS(dt, file.path(folder, paste0(bsn, ".rds")))
+  for (attempt in 1:retries) {
+    result <- try(
+      get_data(
+        table = "Transcript",
+        IdSubject = sbj,
+        Language = "DE"
+      ),
+      silent = TRUE
+    )
+
+    if (!inherits(result, "try-error")) {
+      saveRDS(result, out_file)
+      message("Downloaded ", sbj, " (attempt ", attempt, ")")
+      return(TRUE)
+    }
+
+    message("Error on ", sbj, " (attempt ", attempt, "): ", result)
+    Sys.sleep(wait * attempt) # exponential backoff
+  }
+
+  message("FAILED permanently: ", sbj)
+  write(sbj, file = "failed_ids.txt", append = TRUE)
+  return(FALSE)
 }
 
-plan(multisession, workers = 6)
-future_walk(
-  businesses$BusinessShortNumber,
-  get_subject_business,
+fut <- future_walk(
+  subject_businesses$IdSubject,
+  get_transcripts,
   .progress = TRUE
 )
 
-subject_businesses <- map_dfr(
-  list.files("subject_businesses", full.names = TRUE),
+value(fut)
+
+transcripts <- map_dfr(
+  list.files("Data/transcripts", full.names = TRUE),
   readRDS
 )
-saveRDS(subject_businesses, "Data/subject_businesses_2015_2025.rds")
+saveRDS(transcripts, "Data/transcripts.rds")
 
-# get transcripts per business -------------------------------
-
-businesses_energy <- get_data(
-  table = "Business",
-  Title = "~Klima",
-  Language = "DE"
-)
-
-# TODO: Change Search string to english and include french and italian
-
-# businesses_energy_filtered <- businesses_energy |>
-#   filter(SubmissionDate > as.Date("2025-01-01"))
-
-subject_energy_list <- list()
-for (i in seq_along(businesses_energy$BusinessShortNumber)) {
-  subject_energy_list[[i]] <- get_data(
-    table = "SubjectBusiness",
-    BusinessShortNumber = businesses_energy$BusinessShortNumber[i],
-    Language = "DE"
-  )
-}
-subject_energy <- bind_rows(subject_energy_list)
-
-transcripts_energy_list <- list()
-for (i in seq_along(subject_energy$IdSubject)) {
-  transcripts_energy_list[[i]] <- get_data(
-    table = "Transcript",
-    IdSubject = as.numeric(subject_energy$IdSubject),
-    Language = "DE"
-  )
-}
-transcripts_energy <- bind_rows(transcripts_energy_list)
-saveRDS(transcripts_energy, "Data/transcripts_energy.rds")
+# filter transcripts:
+# only debates after 1.1.2025
+# debates around climate change
